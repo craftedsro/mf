bfac17a012eb56d8ff3f1fa89d9a444b32bfad62
ska266miljak <M.Counsultant@consultant.crif.com>
2024-04-26 15:24:31 +0200

    remove node_modules

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,12 @@
+import {mount} from "packages/marketing/MarketingApp";
+import React, {useEffect, useRef} from "react";
+
+export default () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  }, []);
+
+  return <div ref={ref}/>;
+};
